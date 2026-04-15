87a983c40d747b643ea3d08a08ff8fb079fb6307
DOK-0727 <han889824@gmail.com>
2026-04-15 20:34:52 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,18 +1,31 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
+
+# ==============================
+# 과목 데이터
+# ==============================
+jokbo = {
+    "컴퓨터공학개론": "ㅇㄱㄹ",
+    "C프로그래밍": "ㅇㅂㄱ",
+    "데이터베이스": "ㅅㅎㅅ"
+}
+
+jokbo_list = list(jokbo.items())
+
+for idx, (subject, professor) in enumerate(jokbo_list, start=1):
+    print(idx, subject, professor)
+
+n = int(input("번호를 입력해주세요:"))
 
 chrome_path = "/Users/handokyung/Desktop/Python/chromedriver-mac-arm64/chromedriver"
 service = Service(chrome_path)
 driver = webdriver.Chrome(service=service)
 
-url = "https://everytime.kr"
-driver.get(url)
-
+driver.get("https://everytime.kr")
 wait = WebDriverWait(driver, 15)
 
 def login():
@@ -38,12 +51,35 @@ def login():
 
     wait.until(lambda d: "login" not in d.current_url)
 
-def search():
-    search_form = driver.find_element(By.CSS_SELECTOR, "form.search ")
-    search_input = search_form.find_element(By.CSS_SELECTOR, "input")
+def write(subject, professor):
+    driver.get("https://everytime.kr/370471")
 
-    search_input.send_keys("ㅇㄱㄹ", Keys.ENTER)
+    wait.until(
+        EC.element_to_be_clickable((By.LINK_TEXT, "새 글을 작성해주세요!"))
+    ).click()
+
+    wait.until(
+        EC.presence_of_element_located((By.NAME, "title"))
+    ).send_keys(f"{subject} ({professor} 교수님) 족보 있습니다")
+
+    wait.until(
+        EC.presence_of_element_located((By.NAME, "text"))
+    ).send_keys(f"{subject} ({professor} 교수님) 족보 있습니다. 쪽지주세요.")
+
+    wait.until(
+        EC.element_to_be_clickable((By.CSS_SELECTOR, "li.anonym"))
+    ).click()
+
+    wait.until(
+        EC.element_to_be_clickable((By.CSS_SELECTOR, "li.submit"))
+    ).click()
+
+def search():
+    titles = driver.find_elements(By.CSS_SELECTOR, "h2.medium bold")
+    texts = driver.find_elements(By.CSS_SELECTOR, "p.medium bold")
 
 login()
-search()
+time.sleep(3)
+subject, professor = jokbo_list[n-1]
+write(subject, professor)
 time.sleep(5)
